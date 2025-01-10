e1e5e62b5bd776ead47efe6cac7f8fd5c65c9d35
RAJAOBSON RAKOTONARIVO MANOELA HARDY <hardyrakoto66@gmail.com>
2025-01-10 11:39:09 +0100

    project initial

---
+++ apps/my-app-node/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'my-app-node',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/my-app-node',
+};
